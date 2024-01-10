177046732516154c68ab263f6e3d77b2eed21d6b
Mateus-Santos <mateusantosms11@gmail.com>
2024-01-10 08:29:20 -0300

    Versão da tela de cadastro de pessoas finalizada.

--- resources/views/cadastropessoa.blade.php
+++ resources/views/cadastropessoa.blade.php
@@ -2,82 +2,108 @@
 
 @section('content')
 
-<footer>
   <div class="container cadastro">
 
   <h1>CADASTRO DE PESSOA FÍSICA</h1>
-  <form>
+
+  <form class="row g-3">
       <div class="campos">
-      <div class="form-group">
-        <label for="nomecompleto">Nome Completo:</label>
+      <div class="row mb-3">
+
+        <div class="col-md-6">
+        <label class="form-label" for="nomecompleto">Nome Completo:</label>
         <input type="text" class="form-control" id="nomecompleto" placeholder="Digite seu Nome Completo.">
-      </div>
-
-      <div class="form-row">
-
-        <div class="form-group col-md-6">
-          <label for="cpf">CPF:</label>
-          <input type="text" class="form-control" id="cpf" placeholder="Digite seu CPF.">
         </div>
 
-        <div class="form-group col-md-6">
-          <label for="rg">RG:</label>
-          <input type="text" class="form-control" id="rg" placeholder="Digite o seu RG.">
+        <div class="col-md-6">
+            <label class="form-label" for="email">E-mail</label>
+            <input type="email" class="form-control" id="email" placeholder="Digite o seu Email.">
+        </div>
+        
+      </div>
+
+      <div class="row mb-3">
+
+          <div class="col-md-3"> 
+            <label class="form-label" for="rg">RG:</label>
+            <input type="text" class="form-control" id="rg" placeholder="0123456-7">
+          </div>
+
+          <div class="col-md-2">          
+            <label class="form-label" for="cpf">CPF:</label>
+            <input type="text" class="form-control" id="cpf" placeholder="999.999.999-99">
+          </div>
+        
+        <div class="col-md-3">
+          <label class="form-label" for="tell1">Telefone Principal:</label>
+          <input type="text" class="form-control" id="tell1" placeholder="(99) 9 9999-9999">
+        </div>
+
+        <div class="col-md-3">
+          <label class="form-label" for="tell2">Telefone Secundario:</label>
+          <input type="text" class="form-control" id="tell2" placeholder="(99) 9 9999-9999">
         </div>
 
       </div>
+      <div class="row mb-3">
+        <div class="col-2">
+          <label class="form-label" for="cep">CEP:</label>
+          <input type="text" class="form-control" id="cep" placeholder="00000-000">
+        </div>
 
-      <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" class="form-control" id="email" placeholder="Digite o seu Email.">
+      <div class="col">
+        <label class="form-label" for="cidade">Cidade:</label>
+        <input type="text" class="form-control" id="cidade" placeholder="Exemplo: Belmonte, Serraville, Tranquilópolis, Rio Verde...">
       </div>
 
-      <div class="form-row">
-      <div class="form-group col-md-4">
-        <label for="cep">CEP:</label>
-        <input type="text" class="form-control" id="cep">
-      </div>
-
-      <div class="form-group col-md-6">
-        <label for="cidade">Cidade:</label>
-        <input type="text" class="form-control" id="cidade">
-      </div>
-
-      <div class="form-group col-md-2">
-        <label for="estado">Estado:</label>
+      <div class="col-md-2">
+        <label class="form-label" for="estado">Estado:</label>
         <select id="estado" class="form-control">
           <option selected>Escolher...</option>
           <option>BA</option>
         </select>
       </div>
 
-      <div class="form-group col-md-6">
-        <label for="rua">Rua:</label>
+      <div class="col-md-5">
+        <label class="form-label" for="rua">Rua:</label>
         <input type="text" class="form-control" id="rua">
       </div>
 
-      <div class="form-group col-md-2">
-        <label for="rua">Número:</label>
-        <input type="text" class="form-control" id="rua">
       </div>
 
+      <div class="row mb-3">
+        <div class="col-md-5">
+          <label class="form-label" for="endereco">Endereço:</label>
+          <input type="text" class="form-control" id="endereco" placeholder="Apartamento, hotel, casa, etc.">
+        </div>
+        
+        <div class="col-md-1">
+          <label class="form-label" for="numero">Número:</label>
+          <input type="text" class="form-control" id="numero">
+        </div>
     </div>
     
-    <div class="form-group">
-      <label for="inputAddress2">Endereço:</label>
-      <input type="text" class="form-control" id="Endereço" placeholder="Apartamento, hotel, casa, etc.">
-    </div>
+      <div class="row mb-3">
+        <div class="col">
+          <label class="form-label" for="pont_ref">Ponto de referência:</label>
+          <input type="text" class="form-control" id="pont_ref" placeholder="Exemplo: Próximo ao restaurante, loja, igreja...">
+        </div>
+      </div>
 
-    <div class="form-group">
-      <div class="form-check">
+    <div class="row mb-3">
+      <div class="col">
         <input class="form-check-input" type="checkbox" id="confirmacao">
-        <label class="form-check-label" for="confirmacao">
+        <label for="confirmacao">
           Confirmo que toda as informações adicionadas são verdadeiras.
         </label>
       </div>
     </div>
+
+    <div class="col text-center">
+        <button type="submit" class="btn btn-success">Cadastrar</button>
+    </div>
+
   </div>
-  <button type="submit" class="btn btn-success">Entrar</button>
 </form>
 </div>
 @endsection
