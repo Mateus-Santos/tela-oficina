c245f202941164242dcb41f4c353b9539601dbd2
Bruno Lima <bru.no@outlook.com.br>
2024-10-17 12:33:30 -0300

    Criação da página de erro 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+
+<main id="main">
+
+<section id="erro404" class="erro404">
+<div class="container">
+        <h1>403 - Acesso Negado</h1>
+        <p>Você precisa estar logado para acessar esta página.</p>
+        <a href="{{ route('login') }}">Ir para a página de login</a>
+    </div>
+</section>
+
+@endsection
