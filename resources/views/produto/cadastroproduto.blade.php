f3fff4daa943d0e74462c85e4f831004b7f9ec2d
Mateus-Santos <mateusantosms11@gmail.com>
2026-01-31 22:41:22 -0300

    Correcao em cadastro de produto para preview da imagem do produto

--- resources/views/produto/cadastroproduto.blade.php
+++ resources/views/produto/cadastroproduto.blade.php
@@ -68,9 +68,25 @@
                 </div>
 
                 <div class="col-md-4">
-                    <label class="form-label" for="img">Imagem:</label>
-                    <input type="file" class="form-control" id="img" name="img">
+                <label class="form-label" for="img">Imagem:</label>
+                {{-- Preview da imagem selecionada --}}
+                <div class="mb-2">
+                    <img 
+                        id="img-preview"
+                        style="display:none; max-width: 120px; border-radius: 10px;"
+                        alt="Preview da imagem"
+                    >
                 </div>
+
+                <input 
+                    type="file" 
+                    class="form-control" 
+                    id="img" 
+                    name="img"
+                    accept="image/*"
+                >
+            </div>
+
 
             </div>
 
