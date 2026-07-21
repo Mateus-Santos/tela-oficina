44bea279d41f4dbd070bf985e1788047bb1fb6ed
Mateus-Santos <mateusantosms11@gmail.com>
2026-07-21 16:40:52 -0300

    Seed agora adaptado para pessoa e user

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -5,25 +5,35 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Pessoa;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class AdminUserSeeder extends Seeder
 {
-
     public function run(): void
     {
-        User::updateOrCreate(
-            ['email' => env('ADMIN_EMAIL')],
-            [
-                'name' => env('ADMIN_NAME'),
-                'password' => Hash::make(env('ADMIN_PASSWORD')),
-                'colaborador' => intval(env('ADMIN_COLABORADOR')),
-                'permitions' => intval(env('ADMIN_PERMITIONS')),
-                'data_nascimento' => env('ADMIN_DATE'),
-                'telefone_1' => env('ADMIN_TELL'),
-            ]
-        );
+        DB::transaction(function () {
+            $user = User::where('email', env('ADMIN_EMAIL'))->first();
+
+            $pessoa = Pessoa::updateOrCreate(
+                ['id' => $user?->pessoa_id],
+                [
+                    'nome'            => env('ADMIN_NAME'),
+                    'data_nascimento' => env('ADMIN_DATE'),
+                    'telefone_1'      => env('ADMIN_TELL'),
+                ]
+            );
+
+            User::updateOrCreate(
+                ['email' => env('ADMIN_EMAIL')],
+                [
+                    'pessoa_id'   => $pessoa->id,
+                    'password'    => Hash::make(env('ADMIN_PASSWORD')),
+                    'colaborador' => intval(env('ADMIN_COLABORADOR')),
+                    'permitions'  => intval(env('ADMIN_PERMITIONS')),
+                ]
+            );
+        });
     }
 }
