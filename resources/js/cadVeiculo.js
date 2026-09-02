a08da5eac3573887fed98ad1ce07168ed226b247
Mateus Santos <mateusantosms11@gmail.com>
2026-09-02 10:43:58 -0300

    Fix: Relizada correcoes de rotas e api de retorno de OS dos veiculos para cadastro em nota

--- resources/js/cadVeiculo.js
+++ resources/js/cadVeiculo.js
@@ -1,29 +1,40 @@
 $('#montadora').on('change', function () {
+
     var montadoraId = $(this).val();
 
-    $('#veiculo_id').empty().append('<option>Carregando...</option>');
+    $('#veiculo_id')
+        .empty()
+        .append('<option>Carregando...</option>');
 
     if (montadoraId) {
 
-        $.get('/veiculos/montadora/' + montadoraId, function (data) {
+        $.get('/api/montadora/' + montadoraId + '/veiculos', function (data) {
 
-            $('#veiculo_id').empty().append('<option value="">Selecione um veículo</option>');
+            $('#veiculo_id')
+                .empty()
+                .append('<option value="">Selecione um veículo</option>');
 
             data.forEach(function (v) {
 
-                var selected = v.id == veiculoSelecionado ? 'selected' : '';
+                var selected = v.id == veiculoSelecionado
+                    ? 'selected'
+                    : '';
 
                 $('#veiculo_id').append(
                     `<option value="${v.id}" ${selected}>${v.nome}</option>`
                 );
             });
+
         });
 
     } else {
-        $('#veiculo_id').empty().append('<option value="">Selecione a montadora primeiro</option>');
+
+        $('#veiculo_id')
+            .empty()
+            .append('<option value="">Selecione a montadora primeiro</option>');
     }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('#montadora').trigger('change');
 });
